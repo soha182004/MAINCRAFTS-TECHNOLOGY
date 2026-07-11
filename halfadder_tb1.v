8eb535e898a1e9d8734db37c4898e3bd076d7dc9
soha182004 <sohakalekhan@gmail.com>
2026-07-11 22:02:41 +0530

    Add testbench for half adder module

--- halfadder_tb1.v
+++ halfadder_tb1.v
@@ -1,1 +1,24 @@
+module tb_half_adder;
 
+reg A, B;
+wire Sum, Carry;
+
+half_adder uut(
+    .A(A),
+    .B(B),
+    .Sum(Sum),
+    .Carry(Carry)
+);
+
+initial begin
+
+A = 0; B = 0; #10;
+A = 0; B = 1; #10;
+A = 1; B = 0; #10;
+A = 1; B = 1; #10;
+
+$finish;
+
+end
+
+endmodule
